0ce7ff2a9c6d6116b21ec04a0c19d526d6cc62e0
Dong-kyo Jeong <ehdrylang@naver.com>
2022-10-11 21:53:46 +0900

    bugfix : 잘못된 클래스 import-BindException
    java.net은 필요없고 파라미터 바인딩 에러를 잡기 위하여 spring validation의 예외가 들어갔어야하는데 잘못하여 수정

--- src/main/java/com/example/blog/common/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/blog/common/controller/GlobalExceptionHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.validation.ConstraintViolationException;
-import java.net.BindException;
+import org.springframework.validation.BindException;
 
 @ControllerAdvice
 @Slf4j
